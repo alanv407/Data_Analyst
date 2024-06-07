ef38c27bb7e53350e554a078d772e421e9d87fa0
alanv407 <68298659+alanv407@users.noreply.github.com>
2024-06-06 22:08:50 -0600

    Update 02_👤_About Me.py

--- pages/02_👤_About Me.py
+++ pages/02_👤_About Me.py
@@ -1,89 +1,120 @@
-#Librerias
+#Libreries
 import streamlit as st
 import pandas as pd
-import plotly.express as px
-from PIL import Image
 
-#Configurarción Páginas
+#CPage Configuration
 st.set_page_config(
-	page_title = 'Case Analysis',
-	page_icon = '🛒',
+	page_title = 'About Me',
+	page_icon = ' 👨',
+   initial_sidebar_state="expanded"
 	)
 
-st.title('***Arturo Vargas***')
-st.title('About Me')
+st.title('**Arturo Vargas**')
 
-with st.expander("Know a bit more"):
-    st.write("""
-        I am the kind of person who is not afraid of challenges, I like to bring innovation and motivation to the work team. \n 
-        I describe myself as a dynamic, analytical person, easy to learn and results-oriented. \n
+st.header("Data & Analytics | Data Analyst")
+
+st.divider()
+
+st.subheader('About Me')
+
+with st.expander("Summary"):
+    st.markdown("""
+         - Highly motivated and results-oriented professional with 4 years of experience in Data Science. Proven ability to leverage strong analytical skills and attention to detail. Adept at working collaboratively in fast-paced environments.
+         - Experience in Data Analyst, Data Management (MDM) and Data Steward.
     """)
 
 
-st.header('Skills')
+st.subheader('Skills')
 
 with st.expander("Skills"):
-    st.write("""
-        I have experience handling and interpreting data. \n
-        The tools that I use most are; SQL, Power BI, Python and Excel.
+    st.markdown("""
+                I have experience handling and interpreting data.
+                
+                The tools that I use most are:
+                  
+                  - SQL (Snowflake ❄️, MS Server, Redash)
+                  - 🐍 Python (Jupyter Notebook, Pandas, Numpy, MatplotLib, Plotly, Streamlit)
+                  - Power BI
+                  - MS Office: Word, Excel, Power Point
     """)
 
-st.header('Previous jobs')
+st.divider()
 
-tab6, tab1, tab2, tab3, tab4, tab5 = st.tabs(["Nissan", "IQVIA", "Rappi", "MexWürst", "Wonder World Media",'Antoeli'])
+st.header('Experience')
 
-with tab6:
-   st.subheader("Data Specialist")
-   st.caption('2023')
-   st.markdown('- Gather and process information from various internal and external sources of customer and marketing data.')
-   st.markdown('- Mantain access to the data, ingest, process, cleanse, format, store and distribute data.')
-   st.markdown('Validation of vendors ETL´s process')
-	
-with tab1:
-   st.subheader("Data Analyst")
-   st.caption('2021 - 2022')
-   st.markdown('- Analysis of the integrity of the Database')
-   st.markdown('- Use of databases(SSIS, SQL, Access)')
-   st.markdown('- ETL development (Extraction, Transformation and Load)')
-   st.markdown('- Market analysis and commercial indicators, incentive analysis for sales force')
-   st.markdown('- Analysis and design of reports for general management and clients (Power BI)')
-   #st.markdown('')
-with tab2:
-   st.subheader("Retail CPG´s Analyst")
-   st.caption('2019 - 2020')
-   st.markdown('- QA Retail CPGs')
-   st.markdown('- Assembly of stores, products and prices in application and Web (CMS)')
-   st.markdown('- Updating and debugging of database (SQL)')
-   st.markdown('- Implementation of Agile Scrum methodology (reduction of backlog, completion time and delivery of tasks, reduction of errors in promotions)')
+with st.container(border=True):
 
-with tab3:
-   st.subheader("Marketing scientist")
-   st.caption('2018 - 2019')
-   st.markdown('- Data Mining')
-   st.markdown('- Data visualization')
-   st.markdown('- Creation and execution digital marketing strategies (RRSS, web, SEO, SEM, Blogs, app). ')
+   tab5, tab4, tab3, tab2, tab1 = st.tabs(["Nissan", "IQVIA", "Rappi", "MexWürst", "Wonder World Media"])
 
-with tab4:
-   st.subheader("Internship 🇪🇸")
-   st.caption('2017 - 2018')
-   st.markdown('- Assistant in the sales process')
-   st.markdown('- Management of various events for the promotion of the brand')
-   st.markdown('- Management of clearance sales events')
+   with tab5:
+      st.subheader("Sr. Data Specialist")
+      st.caption('2023 - today')
+      st.markdown("""
+                  - Gather and process information from various internal and internal sources of customer and marketing data.
+                  - Perform validation processes to ensure data accuracy, completeness, and consistency.
+                  - Identify and analyze problems, errors, defects, and other problems in the database.
+                  - Responsible for ongoing data quality & data cleansing initiatives to fix identified issues.
+                  """)
+      
+   with tab4:
+      st.subheader("Data Analyst")
+      st.caption('2021 - 2022')
+      st.markdown("""
+                  - Analysis and design of reports for general management and clients (Power BI, Python).
+                  - Automation of data movements and their transformation, ETL (Extraction, Transformation and Load).
+                  - Market analysis and commercial indicators.
+                  - Price analysis, market penetration, new product launch, market share, growth elements, etc.
+                  - Incentive analysis for sales force.
+                  - Solve doubts from strategic clients about our data.
+                  - Use of databases (SSIS, MS Server SQL, Access, Python).
+                  """)
+      
+   with tab3:
+      st.subheader("Retail  Analyst")
+      st.caption('2019 - 2020')
+      st.markdown("""
+                  - Updating and debugging of database (Redase SQL).
+                  - QA Retail CPG’s.
+                  - Assembly of stores, products and prices in application and Web (Internal CMS).
+                  - Implementation of Scrum Agile reducing errors and backlog.
+                  """)
+
+   with tab2:
+      st.subheader("Marketing Scientist")
+      st.caption('2018 - 2019')
+      st.markdown("""
+                  - Data Mining
+                  - Data visualization
+                  - Creation and execution digital marketing strategies (RRSS, web, SEO, SEM, Blogs, app).""")
+
+   with tab1:
+      st.subheader("Internship 🇪🇸")
+      st.caption('2017 - 2018')
+      st.markdown('- Assistant in the sales process')
+      st.markdown('- Management of various events for the promotion of the brand')
+      st.markdown('- Management of clearance sales events')
+
+st.divider()
 
 st.header('Education')
-tab5, tab6, tab7 = st.tabs(['Máster 🇪🇸','Bachelor 🇲🇽','Diploma'])
 
-with tab5:
-	st.subheader('Marketing Management')
-	st.markdown('EAE BUSINESS SCHOOL / UNIVERSITAT POLITÈCNICA DE CATALUNYA')
-	st.caption('2017 - 2018')
+with st.container(border=True):
 
-with tab6:
-	st.subheader('Chemical Engineering ')
-	st.markdown('Universidad La Salle')
-	st.caption('2012 - 2016')
+   tab5, tab6, tab7 = st.tabs(['Máster 🇪🇸','Bachelor 🇲🇽','Diploma'])
 
-with tab7:
-	st.subheader('Big Data')
-	st.markdown('Fundación Carlos Slim / SEP')
-	st.caption('527 horas / 2021')
+   with tab5:
+      st.subheader('Marketing Management')
+      st.markdown('''
+                  EAE Business School / Universitat Politècnica de Catalunya
+                  Attended in Barcelone Spain''')
+      st.caption('2017 - 2018')
+
+   with tab6:
+      st.subheader('Chemical Engineering ')
+      st.markdown('Universidad La Salle (ULSA)')
+      st.caption('2012 - 2016')
+
+   with tab7:
+      st.subheader('Big Data')
+      st.markdown('Fundación Carlos Slim / SEP')
+      st.caption('527 horas / 2021')
