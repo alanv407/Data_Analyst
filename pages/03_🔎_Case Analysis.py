52699993dde501e55428a405f40fdc1e00ad3f71
alanv407 <68298659+alanv407@users.noreply.github.com>
2024-07-29 12:58:18 -0600

    Update 03_🔎_Case Analysis.py

--- pages/03_🔎_Case Analysis.py
+++ pages/03_🔎_Case Analysis.py
@@ -213,17 +213,16 @@ st.subheader('Next step for DS analysis')
 
 with st.expander("Findings"):
     st.write("""
-        - Only the city of Saltillo achieves the Delivery Success goal, Jalapa almost did it
-        - No city achieves an average in expected shipments
-             - The closest is CDMX 6 shipments below the target
-             - The city with the lowest average shipments is Puebla with 61 shipments on average
-        - The only carrier that achieves the objective is Envios Express, SSJ Serv Logistica, it is very close
-        - If we analyze by cycle_flag, the SP group achieves the DS objective but very far from SPR
-        - The cycle_flag that is closest to SOR is C1, the one that is furthest away is SD
-        - If we analyze by experience, the Experienced category has the best index in DS and SPR
+        - Only the city of Saltillo achieves the Delivery Success goal, Jalapa almost did it.
+        - No city achieves an average in expected shipments.
+             - The closest is CDMX 6 shipments below the target.
+             - The city with the lowest average shipments is Puebla with 61 shipments on average.
+        - The only carrier that achieves the objective is Envios Express, SSJ Serv Logistica, it is very close.
+        - It is proposed to review and validate the Cycle_flag data since outside of SP all have poor performance.
 	- No variable shows any correlation with Delivery Success, so it is assumed that success is an external factor to this information.
        	- According to the graph of boxes by city, the values of cities with SD less than 92 will be taken, where the largest number of atypicals are found, discarding Guadalajara and Merida \n
        	- According to the chart of boxes by carrier, the values of carriers with values less than 88 will be taken, where the greatest number of outliers are found. \n
+	- Experience is not a relevant variable.
     	""")
 
       
