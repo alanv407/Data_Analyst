07e8406db23a9756396268ce1ac1d42783bc3f30
alanv407 <68298659+alanv407@users.noreply.github.com>
2024-07-29 13:33:47 -0600

    Update _🏠_Home.py

--- _🏠_Home.py
+++ _🏠_Home.py
@@ -14,18 +14,18 @@ st.header('Personal Portfolio 💼.')
 
 with st.container(border=True):
     st.subheader('Use of this Web App')
-    st.markdown('On the left side are the different parts of this presentation; Home, About me and Case Analysis, if applicable you will also find some filters.')
+    st.markdown('On the left side are the different parts of this presentation; 🏠 Home, 👤 About me and 🔎 Case Analysis, if applicable you will also find some filters.')
 
 with st.container(border=True):
     st.subheader('More info')
-    st.caption('In some cases you will find a box like the one shown below, by clicking you will see more information')
+    st.markdown('In some cases you will find a box like the one shown below, by clicking you will see more information')
     with st.expander("Know a little more"):
         st.write("""
             This is an example 
         """)
 with st.container(border=True):
     st.subheader('Tabs')
-    st.caption('In other cases you will find Tabs, to see the information of each one you just have to click on each one')
+    st.markdown('In other cases you will find Tabs, to see the information of each one you just have to click on each one')
     tab1, tab2 = st.tabs(['Tab 1','Tab 2'])
     with tab1:
         st.caption('Tab 1')
@@ -35,7 +35,7 @@ with st.container(border=True):
         st.write("""This is another example in Tab 2""")
 	
 with st.container(border=True):    
-    st.markdown('**You can find the code for this Web App GitHub in:** https://github.com/alanv407/Data_Analyst')
+    st.markdown('**You can find the code for this web application on GitHub:** https://github.com/alanv407/Data_Analyst')
 
 if st.button("Let's get started!"):
     st.toast('Hip!')
